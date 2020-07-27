4a536d74de29bbe08ec806e03d1aac95f3d0fcd8
eqbit <eqbits@gmail.com>
2020-07-27 15:16:23 +0500

    implemented basic logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
 import './styles/index.scss';
-import React, { useState } from 'react';
-import { Life } from './Life';
-
-const SIZE = 20;
-const life = new Life({
-  size: SIZE
-});
+import React from 'react';
+import Layout from './components/Layout';
 
 function App() {
-  const [items, setItems] = useState<boolean[]>(life.getField());
-
-  console.log(items);
-
   return (
-    <div className="app">
-      {items.map((square, index) => {
-        return (
-          <div className={`square ${square ? 'square--alive' : 'square--dead'}`} key={`square-${index}`}/>
-        )
-      })}
-    </div>
+    <Layout/>
   );
 }
 
--- src/Life/index.ts
+++ src/Life/index.ts
@@ -4,16 +4,74 @@ interface ConstructorOptions {
   size: number;
 }
 
+export type Field = boolean[];
+
 export class Life {
   protected size: number;
-  protected field: boolean[];
+  protected field: Field;
 
   constructor({ size }: ConstructorOptions) {
     this.size = size;
     this.field = new Array(size * size).fill(null).map(() => getRandomChance());
   }
 
-  public getField() {
+  public getInitialField(): Field {
+    return this.field;
+  }
+
+  protected getPointState(pointIndex: number): boolean {
+    return this.field[pointIndex];
+  }
+
+  protected getPointRow(pointIndex: number): number {
+    return Math.floor(pointIndex / this.size);
+  }
+
+  protected getPointCol(pointIndex: number): number {
+    const rows = this.getPointRow(pointIndex);
+    return pointIndex - (this.size * rows);
+  }
+
+  protected isInBound(row: number, col: number): boolean {
+    return row >= 0 && row < this.size && col >= 0 && col < this.size;
+  }
+
+  protected getPointIndex(row: number, col: number): number {
+    return (row * this.size) + col;
+  }
+
+  protected getPointNeighborsNumber(pointIndex: number): number {
+    let neighborsNumber = 0;
+
+    const pointRow = this.getPointRow(pointIndex);
+    const pointCol = this.getPointCol(pointIndex);
+
+    for (let row = -1; row <= 1; row++) {
+      for (let col = -1; col <= 1; col++) {
+        if (row === 0 && col === 0) {
+          continue;
+        }
+
+        const neighborRow = pointCol + row;
+        const neighborCol = pointRow + col;
+
+        if (this.isInBound(neighborRow, neighborCol)) {
+          if (this.getPointState(this.getPointIndex(neighborRow, neighborCol))) {
+            neighborsNumber++;
+          }
+        }
+      }
+    }
+
+    return neighborsNumber;
+  }
+
+  public getRecalculatedField(): Field {
+    this.field = this.field.map((_, index) => {
+      const neighborsNumber = this.getPointNeighborsNumber(index);
+      return neighborsNumber > 1 && neighborsNumber < 4;
+    });
+
     return this.field;
   }
 }
